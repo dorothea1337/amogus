2ee3f6694bb6de00f704e3cdaa4035ff8ef2cc8b
fptitsyn <koteyka_mira@mail.ru>
2024-11-06 10:25:34 +0300

    начинаем

---
+++ самолетик/Assets/Scripts/PlaneController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneController : MonoBehaviour
+{
+    private void FixedUpdate()
+    {
+        var deltatime = Time.fixedDeltaTime;
+        
+        //CalculateState();
+    }
+}
